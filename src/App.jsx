c9c8bf96ee0a3223cbabc26bed43e7671b462646
MorfinWorkin <morfinworking@gmail.com>
2022-11-05 21:54:23 -0600

    Add DaAMT component to file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import DaNav from './components/DaNav';
+import './App.css';
+import DaAMT from './components/DaAMT';
 
 function App() {
   return (
     <>
       <DaNav />
+      <DaAMT bright={10} color={'fff'} />s
     </>
   );
 }
