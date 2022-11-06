e7d29d23cc9a671283c0297b9fffb00d8f481db9
MorfinWorkin <morfinworking@gmail.com>
2022-11-05 21:55:32 -0600

    Change style of styled components.

--- src/components/DaNav.jsx
+++ src/components/DaNav.jsx
@@ -1,28 +1,47 @@
 import React from 'react';
 import styled from 'styled-components';
+
 const DaContainer = styled.div`
   width: 100%;
   margin: auto;
   display: inline-block;
-  background-color: #444444;
+  background-color: #afafaf;
+`;
+const DaNavContainer = styled.nav`
+  margin: auto;
+  height: 100%;
+  padding: 10px;
+  background-color: #afafaf;
 `;
 
-const DaButton = styled.button`
-  width: 5%;
-  padding: 5px;
-  margin: 1%;
-  border: none;
-  height: 60px;
+const DaButton = styled.div`
+  height: 100%;
+  padding-inline: 1.82vw;
+  margin-block: auto;
+  border-radius: 5px;
+  background: #afafaf;
+  font-weight: 500;
 
-  border-radius: 10px;
-  background: #645959;
-  box-shadow: inset 5px -5px 0px #383232, inset -5px 5px 0px #908080;
+  &:hover {
+    border-radius: 10px;
+    background: #afafaf;
+    box-shadow: inset 3px 3px 1px #898989, inset -3px -3px 1px #d6d6d6;
+
+    transition: 0.5s;
+  }
 `;
-const DaNav = () => {
+const DaNav = ({ children }) => {
   return (
-    <DaContainer>
-      <DaButton />;
-    </DaContainer>
+    <>
+      <DaContainer className='fluid sticky-top'>
+        <DaNavContainer className='navbar navbar-expand-lg'>
+          <a className='navbar-brand' href='/'>
+            <DaButton> MORFIN WORKING</DaButton>
+          </a>
+        </DaNavContainer>
+      </DaContainer>
+      {children}
+    </>
   );
 };
 
