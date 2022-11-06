e6c7dad3cc6151081a499ee839efa344731a3af6
MorfinWorkin <morfinworking@gmail.com>
2022-11-06 16:28:05 -0600

    Add carousel component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,37 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import DaNav from './components/DaNav';
 import './App.css';
 import DaAMT from './components/DaAMT';
+import styled from 'styled-components';
+import DaCarousel from './components/DaCarousel/DaCarousel';
+
+const DaDiv = styled.div`
+  padding: ${({ padding }) => (padding ? padding : 0)};
+  margin: auto;
+  background: #bebebe22;
+  border: ${({ border }) => (border ? '1px solid #ffffff00' : '0px')};
+  border-radius: 0px;
+  backdrop-filter: blur(5.1px);
+  -webkit-backdrop-filter: blur(5.1px);
+  color: #fff;
+  height: ${({ height }) => height};
+  width: ${({ width }) => width};
+`;
 
 function App() {
   return (
     <>
       <DaNav />
-      <DaAMT bright={10} color={'fff'} />s
+      <DaAMT bright={90} color={''} />
+      <DaDiv>
+        {/* <p style={{ margin: 'auto', display: 'flex-box' }}>
+            “First, solve the problem. Then, write the code.”
+            <br />
+            <strong>
+              <em> &nbsp; - John Johnson</em>
+            </strong>
+          </p> */}
+        <DaCarousel />
+      </DaDiv>
     </>
   );
 }
