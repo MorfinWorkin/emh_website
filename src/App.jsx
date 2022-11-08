a8f21e33283f301fc54202010cf55df218c0781f
MorfinWorkin <morfinworking@gmail.com>
2022-11-07 18:15:34 -0600

    Add cv png file

--- src/App.jsx
+++ src/App.jsx
@@ -3,37 +3,37 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import DaNav from './components/DaNav';
 import './App.css';
 import DaAMT from './components/DaAMT';
-import styled from 'styled-components';
 import DaCarousel from './components/DaCarousel/DaCarousel';
-
-const DaDiv = styled.div`
-  padding: ${({ padding }) => (padding ? padding : 0)};
-  margin: auto;
-  background: #bebebe22;
-  border: ${({ border }) => (border ? '1px solid #ffffff00' : '0px')};
-  border-radius: 0px;
-  backdrop-filter: blur(0px);
-  -webkit-backdrop-filter: blur(0px);
-  color: #fff;
-  height: ${({ height }) => height};
-  width: ${({ width }) => width};
-`;
+import { Container, Row } from 'react-bootstrap';
+import myCV from './assets/myCV.png';
 
 function App() {
   return (
     <>
-      <DaNav />
-      <DaAMT bright={90} color={''} />
-      <DaDiv>
-        {/* <p style={{ margin: 'auto', display: 'flex-box' }}>
-            “First, solve the problem. Then, write the code.”
-            <br />
-            <strong>
-              <em> &nbsp; - John Johnson</em>
-            </strong>
-          </p> */}
+      <Container className='fluid m-0 p-0'>
+        <DaAMT bright={0} color={''} />
         <DaCarousel />
-      </DaDiv>
+      </Container>
+      <h1
+        style={{
+          position: 'absoulte',
+          display: 'flex',
+          justifyContent: 'center',
+          padding: '5vh',
+          fontSize: '1.5em',
+        }}
+      >
+        “First, solve the problem. Then, write the code.”
+        <br />
+        <strong>
+          <em> &nbsp; - John Johnson</em>
+        </strong>
+      </h1>
+      <Container contain>
+        <Row>
+          <img alt='Eduardo Morfin CV' src={myCV} />
+        </Row>
+      </Container>
     </>
   );
 }
