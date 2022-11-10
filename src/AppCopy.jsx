05abb940b4d7aa852a3355accf722e08b1905d9e
MorfinWorkin <morfinworking@gmail.com>
2022-11-09 20:10:33 -0600

    Change DaCv import path

--- src/AppCopy.jsx
+++ src/AppCopy.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import myCV from './assets/myCV.png';
-import DaCV from './components/DaCV';
+import DaCV from './components/DaCV/DaCV';
 import { Container } from 'react-bootstrap';
 
 function App() {
