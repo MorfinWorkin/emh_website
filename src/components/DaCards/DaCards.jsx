92d6dc45efb60fec2bc81bbff9f03874f62b42e9
MorfinWorkin <morfinworking@gmail.com>
2022-11-16 14:16:59 -0600

    Change DaCard styles and functionality.
    - add imports for certificate images
    - display certificate image when active
    - set draggable prop to false

--- src/components/DaCards/DaCards.jsx
+++ src/components/DaCards/DaCards.jsx
@@ -5,40 +5,66 @@ import logoJS from '../../assets/JSlogo.svg';
 import logoReact from '../../assets/ReactBadge.svg';
 import logoCSS from '../../assets/CSSlogo.svg';
 import logoThree from '../../assets/ThreeJSlogo.svg';
-import { Col, Container, Row } from 'react-bootstrap';
+import jsCer from '../../assets/certificateJS.webp';
+import reactCer from '../../assets/certificateReact.webp';
+import cssCer from '../../assets/certificateCSS.webp';
+import wip from '../../assets/inProgress.webp';
+import { Col, Row } from 'react-bootstrap';
 
 const DaCard = styled.div`
   width: 100%;
   height: ${({ height }) => height};
-  background-color: ${({ bgColor }) => bgColor};
+  background-color: ${({ bgColor }) => `${bgColor}99`};
   display: flex;
   justify-content: center;
   align-items: center;
   border-radius: 25px;
   margin-top: 5%;
+  z-index: 1;
 `;
 
-const CardImg = styled.img` 
-    max-width: 100%;
-    height: 100%;   
-    transition: all 0.3s;
-    transform: scale(.9);
-    src: ${(src) => (src ? src : '')}
-    &:hover {  
-      transform: scale(1.1);
+const CardImg = styled.img`
+  max-width: 100%;
+  height: 100%;
+  z-index: 0;
+  transition: transform 0.3s;
+  transform: scale(0.8);
+  border-radius: 25px;
+  &:active {
+    transform: scale(1);
+    position: absolute;
+    width: 85%;
+    height: 85%;
+    margin-inline: 8%;
+    padding: 2%;
+    top: 10%;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #bebebe;
+    z-index: 0;
+    border-radius: 25px;
+    &:nth-child(1) {
+      z-index: 1;
+      border-radius: 25px;
+      background-repeat: no-repeat;
+      background-position: center;
+      background-size: contain;
+      background-color: #ffffff;
+      background-image: ${({ bgHover }) =>
+        bgHover ? `url(${bgHover})` : `url(${wip})`};
+      object-position: -99999px 99999px;
     }
-  
+  }
 `;
 const DaCards = () => {
-  // const [orientation, setOrientation] = useState(initialState);
   const orientation = window.screen.orientation.type;
   const vertical = 'portrait-primary';
-  /* horizontal  width: 100%;  height: 80%;*/
   const calcOrientation = orientation === vertical ? '15vh' : '75vh';
-
   return (
     <>
       <Row
+        id='myHeader'
         style={{
           justifyContent: 'center',
           alignItems: 'center',
@@ -48,28 +74,53 @@ const DaCards = () => {
         }}
       >
         <Col md={2}>
-          <DaCard height={calcOrientation} id='card-1' bgColor='#61DAFF99'>
-            <CardImg src={logoReact} />
+          <DaCard
+            draggable={false}
+            height={calcOrientation}
+            id='card-1'
+            bgColor='#61DAFF'
+          >
+            <CardImg draggable={false} bgHover={reactCer} src={logoReact} />
           </DaCard>
         </Col>
         <Col md={2}>
-          <DaCard height={calcOrientation} id='card-2' bgColor='#E44D2695'>
-            <CardImg src={logoHTML5} />
+          <DaCard
+            draggable={false}
+            height={calcOrientation}
+            id='card-2'
+            bgColor='#E44D26'
+          >
+            <CardImg draggable={false} src={logoHTML5} />
           </DaCard>
         </Col>
         <Col md={2}>
-          <DaCard height={calcOrientation} id='card-3' bgColor='#F0BE2595'>
-            <CardImg src={logoJS} />
+          <DaCard
+            draggable={false}
+            height={calcOrientation}
+            id='card-3'
+            bgColor='#F0BE25'
+          >
+            <CardImg draggable={false} bgHover={jsCer} src={logoJS} />
           </DaCard>
         </Col>
         <Col md={2}>
-          <DaCard height={calcOrientation} id='card-4' bgColor='#264DE495'>
-            <CardImg src={logoCSS} />
+          <DaCard
+            draggable={false}
+            height={calcOrientation}
+            id='card-4'
+            bgColor='#264DE4'
+          >
+            <CardImg draggable={false} bgHover={cssCer} src={logoCSS} />
           </DaCard>
         </Col>
         <Col md={2}>
-          <DaCard height={calcOrientation} id='card-5' bgColor='#bbbbbb99'>
-            <CardImg src={logoThree} />
+          <DaCard
+            draggable={false}
+            height={calcOrientation}
+            id='card-5'
+            bgColor='#bbbbbb'
+          >
+            <CardImg draggable={false} src={logoThree} />
           </DaCard>
         </Col>
       </Row>
