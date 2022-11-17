d1e3100d7add8a959a96debec30f8875d12a6f16
MorfinWorkin <morfinworking@gmail.com>
2022-11-17 17:46:59 -0600

    remove firebase obj config.

--- src/index.js
+++ src/index.js
@@ -5,22 +5,6 @@ import App from './App.jsx';
 import reportWebVitals from './reportWebVitals';
 import DaNav from './components/DaNav';
 import DaFooter from './components/DaFooter';
-import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyAYu4k-K-TtKq5mNk_nJy2vsfevDbFXP1I',
-  authDomain: 'emorfincv.firebaseapp.com',
-  projectId: 'emorfincv',
-  storageBucket: 'emorfincv.appspot.com',
-  messagingSenderId: '752423824566',
-  appId: '1:752423824566:web:89eb9d3159fb8f9f3d8a17',
-  measurementId: 'G-BW0507460F',
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
